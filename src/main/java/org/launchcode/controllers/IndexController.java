ca4015eb31cfa2c9d6ceb1aa84c0a6e1fb3cd963
Brendan <mcb0193@comcast.net>
2017-04-19 19:54:24 -0500

    Add basic request mappings for the index and new post routes

--- src/main/java/org/launchcode/controllers/IndexController.java
+++ src/main/java/org/launchcode/controllers/IndexController.java
@@ -1,4 +1,33 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+
+@Controller
 public class IndexController {
+
+    private ArrayList<Post> posts;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model) {
+
+        model.addAttribute("title", "Latest Finds - MusicFinds");
+        model.addAttribute("posts", posts);
+
+        return "index";
+    }
+
+    @RequestMapping(value = "newpost", method = RequestMethod.GET)
+    public String showNewPostForm(Model model) {
+
+        model.addAttribute("title", "Share a Find - MusicFinds");
+
+        return "new-post";
+    }
+
 }
