a305cf2abf12579e7bc1fb812ce8433245a7d68e
Brendan <mcb0193@comcast.net>
2017-04-22 13:36:42 -0500

    Add more functionality to controller

--- src/main/java/org/launchcode/controllers/IndexController.java
+++ src/main/java/org/launchcode/controllers/IndexController.java
@@ -1,23 +1,28 @@
 package org.launchcode.controllers;
 
 import org.launchcode.models.Post;
+import org.launchcode.models.data.PostDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.ArrayList;
+import javax.validation.Valid;
 
 @Controller
 public class IndexController {
 
-    private ArrayList<Post> posts;
+    @Autowired
+    private PostDao postDao;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model) {
 
+        model.addAttribute("posts", postDao.findAll());
         model.addAttribute("title", "Latest Finds - MusicFinds");
-        model.addAttribute("posts", posts);
 
         return "index";
     }
@@ -26,8 +31,25 @@ public class IndexController {
     public String showNewPostForm(Model model) {
 
         model.addAttribute("title", "Share a Find - MusicFinds");
+        model.addAttribute("header", "Share a Find");
+        model.addAttribute(new Post());
 
         return "new-post";
+    }
+
+    @RequestMapping(value = "newpost", method = RequestMethod.POST)
+    public String handleNewPostSubmission(@Valid @ModelAttribute Post postToAdd,
+                                          Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Share a Find - MusicFinds");
+            model.addAttribute("header", "Share a Find");
+            return "new-post";
+        }
+
+        postDao.save(postToAdd);
+
+        return "redirect:";
     }
 
 }
