092175a123d16620e52f682ea7257d219a0c16b1
Brendan <mcb0193@comcast.net>
2017-04-26 22:18:02 -0500

    Set up Many to One relationship with Post

--- src/main/java/org/launchcode/models/Comment.java
+++ src/main/java/org/launchcode/models/Comment.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import javax.persistence.GeneratedValue;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -21,6 +22,9 @@ public class Comment {
     @NotNull
     @Size(min = 1, message = "The author box cannot be empty.")
     private String author;
+
+    @ManyToOne
+    private Post post;              // This field describes which post the comment was made on
 
     public Comment() {}
 
