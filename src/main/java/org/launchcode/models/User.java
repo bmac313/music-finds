dcf3a1c5a7d15f9de9f56d0a89254b14834c2650
Brendan <mcb0193@comcast.net>
2017-05-06 09:35:09 -0500

    Add fields, getters / setters, and some required annotations

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,7 +1,46 @@
 package org.launchcode.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
+    private int id;
 
+    @NotNull
+    @Size(min = 1, message = "Username cannot be empty.")
+    private String userName;
 
+    @NotNull
+    @Size(min = 1, message = "Password cannot be empty.")
+    private String passwordHash;
+
+    private List<Post> posts;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 }
