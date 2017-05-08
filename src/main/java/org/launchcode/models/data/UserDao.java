3eae56dfbfd2ab93483bce205dcbe3d83fc63ac6
Brendan <mcb0193@comcast.net>
2017-05-08 13:34:01 -0500

    Add missing required annotations

--- src/main/java/org/launchcode/models/data/UserDao.java
+++ src/main/java/org/launchcode/models/data/UserDao.java
@@ -2,6 +2,11 @@ package org.launchcode.models.data;
 
 import org.launchcode.models.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface UserDao extends PagingAndSortingRepository<User, Integer> {
 }
--- src/main/java/org/launchcode/models/data/UserSubmissionDao.java
+++ src/main/java/org/launchcode/models/data/UserSubmissionDao.java
@@ -2,6 +2,11 @@ package org.launchcode.models.data;
 
 import org.launchcode.models.UserSubmission;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface UserSubmissionDao extends PagingAndSortingRepository<UserSubmission, Integer> {
 }
