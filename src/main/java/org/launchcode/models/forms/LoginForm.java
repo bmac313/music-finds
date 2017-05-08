b270a13478246cf5ca81517fa34d2be7eaa35bed
Brendan <mcb0193@comcast.net>
2017-05-08 14:48:20 -0500

    Add fields, constructors, and getters / setters to LoginForm

--- src/main/java/org/launchcode/models/forms/LoginForm.java
+++ src/main/java/org/launchcode/models/forms/LoginForm.java
@@ -1,4 +1,38 @@
 package org.launchcode.models.forms;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class LoginForm {
+
+    @NotNull
+    @Size(min = 1, message = "Please enter your username.")
+    private String username;
+
+    @NotNull
+    @Size(min = 1, message = "Please enter your password.")
+    private String password;
+
+    public LoginForm() {}
+
+    public LoginForm(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
