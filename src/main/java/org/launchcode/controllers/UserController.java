98b2b850bbe7cf4afa578a7457ac5a034654f100
Brendan <mcb0193@comcast.net>
2017-05-10 20:05:21 -0500

    Add basic login and account creation features.

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.User;
 import org.launchcode.models.data.UserDao;
 import org.launchcode.models.forms.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -11,8 +12,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+//TODO: add confirmation messages informing the user of a successful signup / login
 @Controller
 public class UserController {
 
@@ -30,15 +34,45 @@ public class UserController {
     }
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
-    public String signInUser(@ModelAttribute(name = "login") @Valid LoginForm loginForm,
-                             Errors errors, Model model) {
+    public String logInUser(@ModelAttribute(name = "login") @Valid LoginForm loginForm,
+                            Errors errors, Model model, HttpServletResponse response) {
         if (errors.hasErrors()) {
             model.addAttribute("title", "Log In - MusicFinds");
             model.addAttribute("header", "Log in to your account");
+            model.addAttribute("wrongLoginInfoError", "");
             return "users/login";
         }
 
-        return "redirect:";
+        int userId = 0;
+
+        for (User user : userDao.findAll()) {
+
+            // If the username and password entered match a user in the database, create cookies for that user.
+            if (loginForm.getUsername().equals(user.getUsername())) {
+                if (loginForm.getPassword().equals(user.getPassword())) {
+                    userId = user.getId();
+
+                    Cookie sessionCookie = new Cookie("loggedIn", "true");
+                    Cookie userNameCookie = new Cookie("username", loginForm.getUsername());
+                    Cookie userPwCookie = new Cookie("password", loginForm.getPassword());
+                    Cookie userIdCookie = new Cookie("id", Integer.toString(userId));
+
+                    response.addCookie(sessionCookie);
+                    response.addCookie(userNameCookie);
+                    response.addCookie(userPwCookie);
+                    response.addCookie(userIdCookie);
+
+                    return "redirect:";
+                }
+            }
+        }
+
+        //If the username or password does not match the database, create an error and return to the login form.
+
+        model.addAttribute("title", "Log In - MusicFinds");
+        model.addAttribute("header", "Log in to your account");
+        model.addAttribute("wrongLoginInfoError", "Incorrect username or password.");
+        return "users/login";
     }
 
     @RequestMapping(value = "signup", method = RequestMethod.GET)
@@ -57,6 +91,8 @@ public class UserController {
             model.addAttribute("header", "Create an account");
             return "users/signup";
         }
+
+        userDao.save(newUser);
 
         return "redirect:";
     }
