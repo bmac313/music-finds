21f5234268be8e23973627a858032878ef94ec6e
Brendan <mcb0193@comcast.net>
2017-05-16 13:56:04 -0500

    Add code that sets up the initial cookies for the site

--- src/main/java/org/launchcode/controllers/IndexController.java
+++ src/main/java/org/launchcode/controllers/IndexController.java
@@ -4,11 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index() {
+    public String index(HttpServletRequest request) {
+        // Set up blank cookies if they do not already exist.
+        if (request.getCookies() == null) {
+            Cookie loggedInCookie = new Cookie("loggedIn", null);
+            Cookie userIdCookie = new Cookie("id", null);
+        }
+
         return "redirect:posts";
     }
 
