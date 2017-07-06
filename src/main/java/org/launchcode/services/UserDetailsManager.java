ffc36f70f0937f50a583f9e036a7af8c3db1399b
Brendan <mcb0193@comcast.net>
2017-07-06 17:15:17 -0500

    Implement loadUserByUsername

--- src/main/java/org/launchcode/services/UserDetailsManager.java
+++ src/main/java/org/launchcode/services/UserDetailsManager.java
@@ -1,9 +1,34 @@
 package org.launchcode.services;
 
+import org.launchcode.models.Role;
+import org.launchcode.models.User;
+import org.launchcode.models.data.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
-public class UserDetailsManager {
+public class UserDetailsManager implements UserDetailsService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findByUsername(username);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for (Role role : user.getRoles()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
 
 }
