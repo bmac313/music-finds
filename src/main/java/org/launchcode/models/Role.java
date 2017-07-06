93c901451f6015560e042b5b9c97b35d4e72af36
Brendan <mcb0193@comcast.net>
2017-07-06 17:13:54 -0500

    Add fields, constructor, and getters / setters to Role

--- src/main/java/org/launchcode/models/Role.java
+++ src/main/java/org/launchcode/models/Role.java
@@ -1,4 +1,47 @@
 package org.launchcode.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+@Entity
 public class Role {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role() {}
+
+    public Role(String name, Set<User> users) {
+        this.name = name;
+        this.users = users;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 }
