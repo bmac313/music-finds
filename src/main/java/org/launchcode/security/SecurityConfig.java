ebfdf4b239c4bf0b3470a8aef97ec37158214288
Brendan <mcb0193@comcast.net>
2017-08-28 15:56:05 -0500

    Create class SecurityConfig

---
+++ src/main/java/org/launchcode/security/SecurityConfig.java
@@ -0,0 +1,7 @@
+package org.launchcode.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
